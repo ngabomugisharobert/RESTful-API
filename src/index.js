72bab9270d445f901cd19fc2cc9adda95fd6f31c
NGABO MUGISHA ROBERT <ngabo.mugisha.bro@gmail.com>
2019-10-05 14:48:20 +0200

    second

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express()
 
+let personRoute = require('./routes/person')
 
+app.use(personRoute)
 app.use(express.static('public'))
 
 const port = process.env.PORT || 4000
+
 app.listen(port, () => console.info(`Server has started on ${port}`))
